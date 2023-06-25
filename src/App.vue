fc2b9121281c5ad57d907577d7e5f37c39552b51
Gustavo André Richter <gustavoarichter@gmail.com>
2023-06-24 22:14:51 -0300

    chore: add componente Button

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
-<template></template>
+<template>
+	<div>
+		<form @submit.prevent="submitForm">
+			<Input placeholder="Digite seu nome" v-model="name" />
+			<Input placeholder="Digite seu e-mail" v-model="email" />
+			<Button @click="submitForm">Enviar</Button>
+		</form>
+	</div>
+</template>
 
 <script>
 import Input from './components/Input.vue';
+import Button from './components/Button.vue';
 
 export default {
 	name: 'App',
 	components: {
+		Input,
+		Button
 	},
 	data() {
 		return {
+			name: '',
+			email: '',
 		};
 	},
 	methods: {
-
+		submitForm() {
+			// Lógica para enviar o formulário
+			console.log('Formulário enviado!');
+		}
 	}
 }
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="handleClick" class="button-component">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    methods: {
+        handleClick() {
+            this.$emit('click');
+        },
+    },
+};
+</script>
+
+<style scoped>
+.button-component {
+    /* Estilos do componente de botão */
+}
+</style>
