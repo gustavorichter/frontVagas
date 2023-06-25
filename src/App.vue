e82d6643b175145e7f0b677b373985d86dd0c51d
Gustavo André Richter <gustavoarichter@gmail.com>
2023-06-24 21:49:02 -0300

    Ajuste no projeto, remoção de exemplo e inicio de input

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
-<template>
-	<img alt="Vue logo" src="./assets/logo.png">
-	<HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue';
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+	},
+	data() {
+		return {
+		};
+	},
+	methods: {
+
 	}
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+	<input v-bind="$attrs" v-on="$listeners" class="input-component" />
+</template>
+
+<script>
+	export default {
+		name: 'Input',
+	};
+</script>
+
+<style scoped>
+	.input-component {
+		/* Estilos do componente de input */
+	}
+</style>
